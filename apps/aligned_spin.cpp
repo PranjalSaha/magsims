7db97ddc78fad8223ea8b1f74a1b33894f5053b3
Pranjal Saha <164633174+PranjalSaha@users.noreply.github.com>
2026-02-06 04:44:43 +0530

    Adding files for sharing repo
    Batch 1 - apps and bin

--- apps/aligned_spin.cpp
+++ apps/aligned_spin.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include <string>
-#include <cmath>
+#include <filesystem>
 #include "Vector.h"
 #include "Matrix.h"
 #include "DateTime.h"
 #include "Simulation.h"
 #include "Satellite.h"
 #include "Shell.h"
+#include "Numerics.h"
 using namespace std;
-
-// NOTE: This simulation is used to run so that we are able to debug
-// The Backend of the project.
-// Here the satellite is spinning in a very aligned way with the magnetic field
-// only acting in the z direction.
-// This will allow us to debug the code easily.
 
 int main()
 {
     // Clearing screen
-    cout << clear_screen_string();
+    cout << clearScreenString();
 
     /* NOTE:
         // -- -- -- -- -- -- -- -- - //
@@ -26,9 +21,72 @@ int main()
         // -- -- -- -- -- -- -- -- - //
     */
 
-    string outputcsvfile = "../results/shared_data.csv";
+    /*
+    // Setting filenames
+    string filename;
+    cout << "Enter name of simulation (without extensions) : ";
+    cin >> filename;
+    cout << endl;
+    string outputCSVFile = "../results/" + filename + ".csv";
+
+    // Ensuring no overwrite
+    bool writePermission = filesystem::exists(outputCSVFile);
+    while (writePermission){
+        cout << "File exists. Do you want to overwrite? [0/1]";
+        cin >> writePermission;
+        if (!writePermission){
+            cout << "Enter name of simulation (without extensions) : ";
+            cin >> filename;
+            string outputCSVFile = "../results/" + filename + ".csv";
+            writePermission = filesystem::exists(filename);
+        }
+    }
+
+    cout << "Writing in to CSV file : " << outputCSVFile << endl;
+    */
+
+    // Setting filenames
+    string filename;
+    string outputCSVFile;
+
+    cout << "\n========================================\n";
+    cout << " Simulation Output Configuration\n";
+    cout << "========================================\n\n";
+
+    cout << "Enter simulation name (without extension): ";
+    cin >> filename;
+
+    outputCSVFile = "../results/" + filename + ".csv";
+
+    // Ensuring no overwrite
+    while (filesystem::exists(outputCSVFile)) {
+        cout << "\n⚠️  File already exists:\n";
+        cout << "   " << outputCSVFile << "\n\n";
+        cout << "Overwrite file? [1 = Yes, 0 = No]: ";
+
+        int overwrite;
+        cin >> overwrite;
+
+        if (overwrite == 1) {
+            break;
+        }
+
+        cout << "\nEnter a new simulation name: ";
+        cin >> filename;
+        outputCSVFile = "../results/" + filename + ".csv";
+    }
+
+    cout << "\n----------------------------------------\n";
+    cout << "Writing output to:\n";
+    cout << "  " << outputCSVFile << "\n";
+    cout << "----------------------------------------\n\n";
+
+    // Inputting data
+    string inputmagfile = "../data/csv/igrf-icrf_55_10d-1s.csv";
+
+    // Setting simulation time details
     DateTime start_time("01 Oct 2025 07:00:00.000");
-    DateTime stop_time("01 Oct 2025 08:00:00.000");
+    DateTime stop_time("01 Oct 2025 23:59:59.000");
     double timestep = 1;       // in seconds
 
 
@@ -36,12 +94,21 @@ int main()
         // -- -- -- -- -- --- //
         // HYSTERESIS DETAILS //
         // -- -- -- -- -- --- //
-   */
+    */
 
-    // For PMAC reference paper
-    double H_c = 12;            // Coercivity
-    double B_r = 0.04;         // Retentivity
-    double B_s = 0.27;         // Saturation
+    /*
+    // From PMAC reference paper
+    double H_c = 1.3381;        // Coercivity
+    double B_r = 6.0618e-4;     // Retentivity
+    double B_s = 0.300;         // Saturation
+    double q_0 = 0;             // Required for defining flatley loop
+    double p   = 2;             // Required for defining flatley loop
+    */
+
+    // HyMu80 values
+    double H_c = 1.59154;       // Coercivity
+    double B_r = 0.35;          // Retentivity
+    double B_s = 8000;          // Saturation
     double q_0 = 0;             // Required for defining flatley loop
     double p   = 2;             // Required for defining flatley loop
 
@@ -62,38 +129,38 @@ int main()
     Vector x = {1, 0, 0};       // Orientation of x axis
     Vector y = {0, 1, 0};       // Orientation of y axis
     Vector z = {0, 0, 1};       // Orientation of z axis
-    Vector angular_velocity = {0.0, 0.0, 0.6};
-    Vector angular_acceleration = {0, 0, 0.00001};
+    Vector angular_velocity = {0.17, 0.17, 0.17};
+    Vector angular_acceleration = {0.00001, 0, 0};
 
     // Data for magnetic moment
-    double bar_m    = 12.0;     // Magnetic moment of bar magnet
-    double hyst_vol = 7.4e-8;   // Volume of one hysteresis rod
+    double bar_m    = 0000;     // Magnetic moment of bar magnet
+    double hyst_vol = 18.4e-8;   // Volume of one hysteresis rod
     double hyst_nd  = 0;        // Demagnetising factor (~0 for L/D > 30 E < 2%)
-    int num_x_hyst  = 3;        // --}
-    int num_y_hyst  = 3;        // --}}--number of hysteresis rods
+    int num_x_hyst  = 9;        // --}
+    int num_y_hyst  = 9;        // --}}--number of hysteresis rods
     int num_z_hyst  = 0;        // --}
 
 
     /* NOTE:
-        // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
-        // GENAERATING MAG FIELD n SIMULATING SATELLITE //
-        // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- //
+        // -- -- -- -- -- -- -- -- -- -- -- -- //
+        // READING DATA n SIMULATING SATELLITE //
+        // -- -- -- -- -- -- -- -- -- -- -- -- //
     */
 
     // Activating Virtual environment
-    venv_activate();
+    // venv_activate();
 
-    vector<MagneticEntry> mag_data;
-    DateTime now = DateTime("01 Oct 2025 06:30:00.000");
-    float T = 900;          // Time period in seconds
-    float A = 25;
-    Vector B = {0,0,0};
-    float B_z = 0.0f;
-    for (int i = 0; i < 1000; i++){
-        B_z = A * sin(2 * 3.1416 * i/ T);
-        B = {B_z, B_z, 0};
-        mag_data.emplace_back(now, B);
+    // Reading and storing magetic fields
+    cout << "Reading Magnetic Field Data..." << endl;
+    SampleDataVector magData;
+    for (int i = 1; i < (start_time - stop_time); i++){
+        double A = 1.5;
+        Vector B = {0, A, 0};
+        magData.addSample(start_time + i, B);
     }
+    magData.sort();
+    cout << "Read magnetic field data." << endl
+                                        << endl;
 
     // Initialising Satellite
     cout << "Initialising Satellite..." << endl;
@@ -105,16 +172,18 @@ int main()
     cout << "Initialised Satellite..." << endl
                                        << endl;
     // Exporting parameters
-    export_params(ahan, outputcsvfile);
+    exportParams(ahan, outputCSVFile);
 
     // Simulating and storing data
-    simulate(ahan, mag_data, timestep, outputcsvfile);
+    // simulate(ahan, magData, start_time, stop_time, timestep, outputCSVFile);
+    simulate(ahan, magData, start_time, stop_time, 1, outputCSVFile,
+             IntegratorType::Euler, true);
 
     // Plotting data
-    command("python3 ../utils/plot.py " + outputcsvfile);
+    command("python3 ../utils/plot.py " + outputCSVFile);
 
     // Closing Virtual environment
-    venv_deactivate();
+    // venv_deactivate();
 
     return 0;
 }
--- apps/main.cpp
+++ apps/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 #include "Vector.h"
 #include "Matrix.h"
 #include "DateTime.h"
@@ -11,7 +12,7 @@ using namespace std;
 int main()
 {
     // Clearing screen
-    cout << clear_screen_string();
+    cout << clearScreenString();
 
     /* NOTE:
         // -- -- -- -- -- -- -- -- - //
@@ -19,11 +20,73 @@ int main()
         // -- -- -- -- -- -- -- -- - //
     */
 
+    /*
+    // Setting filenames
+    string filename;
+    cout << "Enter name of simulation (without extensions) : ";
+    cin >> filename;
+    cout << endl;
+    string outputCSVFile = "../results/" + filename + ".csv";
+
+    // Ensuring no overwrite
+    bool writePermission = filesystem::exists(outputCSVFile);
+    while (writePermission){
+        cout << "File exists. Do you want to overwrite? [0/1]";
+        cin >> writePermission;
+        if (!writePermission){
+            cout << "Enter name of simulation (without extensions) : ";
+            cin >> filename;
+            string outputCSVFile = "../results/" + filename + ".csv";
+            writePermission = filesystem::exists(filename);
+        }
+    }
+
+    cout << "Writing in to CSV file : " << outputCSVFile << endl;
+    */
+
+    // Setting filenames
+    string filename;
+    string outputCSVFile;
+
+    cout << "\n========================================\n";
+    cout << " Simulation Output Configuration\n";
+    cout << "========================================\n\n";
+
+    cout << "Enter simulation name (without extension): ";
+    cin >> filename;
+
+    outputCSVFile = "../results/" + filename + ".csv";
+
+    // Ensuring no overwrite
+    while (filesystem::exists(outputCSVFile)) {
+        cout << "\n⚠️  File already exists:\n";
+        cout << "   " << outputCSVFile << "\n\n";
+        cout << "Overwrite file? [1 = Yes, 0 = No]: ";
+
+        int overwrite;
+        cin >> overwrite;
+
+        if (overwrite == 1) {
+            break;
+        }
+
+        cout << "\nEnter a new simulation name: ";
+        cin >> filename;
+        outputCSVFile = "../results/" + filename + ".csv";
+    }
+
+    cout << "\n----------------------------------------\n";
+    cout << "Writing output to:\n";
+    cout << "  " << outputCSVFile << "\n";
+    cout << "----------------------------------------\n\n";
+
+    // Inputting data
     string inputmagfile = "../data/csv/igrf-icrf_55_10d-1s.csv";
-    string outputcsvfile = "../results/shared_data.csv";
+
+    // Setting simulation time details
     DateTime start_time("01 Oct 2025 07:00:00.000");
-    DateTime stop_time("01 Oct 2025 14:00:00.000");
-    double timestep = 1;       // in seconds
+    DateTime stop_time("01 Oct 2025 13:59:59.000");
+    double timestep = 0.01;        // in seconds
 
 
     /* NOTE:
@@ -32,13 +95,30 @@ int main()
         // -- -- -- -- -- --- //
     */
 
-    // For PMAC reference paper
-    double H_c = 12;            // Coercivity
-    double B_r = 0.004;         // Retentivity
-    double B_s = 0.027;         // Saturation
+    /*
+    // From PMAC reference paper
+    double H_c = 1.3381;        // Coercivity
+    double B_r = 6.0618e-4;     // Retentivity
+    double B_s = 0.300;         // Saturation
     double q_0 = 0;             // Required for defining flatley loop
     double p   = 2;             // Required for defining flatley loop
+    */
 
+    /*
+    // HyMu80 values
+    double H_c = 1.59154;       // Coercivity
+    double B_r = 0.35;          // Retentivity
+    double B_s = 8000;          // Saturation
+    double q_0 = 0;             // Required for defining flatley loop
+    double p   = 2;             // Required for defining flatley loop
+    */
+
+    // PMAC Ferromagnetic values
+    double H_c = 1.59154;       // Coercivity
+    double B_r = 0.35;          // Retentivity
+    double B_s = 0.73;          // Saturation
+    double q_0 = 0;             // Required for defining flatley loop
+    double p   = 2;             // Required for defining flatley loop
 
     /* NOTE:
         // -- -- -- -- -- - //
@@ -56,12 +136,12 @@ int main()
     Vector x = {1, 0, 0};       // Orientation of x axis
     Vector y = {0, 1, 0};       // Orientation of y axis
     Vector z = {0, 0, 1};       // Orientation of z axis
-    Vector angular_velocity = {0.6, 0.6, 0.6};
+    Vector angular_velocity = {0.17, 0.17, 0.17};
     Vector angular_acceleration = {0.00001, 0, 0};
 
     // Data for magnetic moment
     double bar_m    = 12.0;     // Magnetic moment of bar magnet
-    double hyst_vol = 7.4e-8;   // Volume of one hysteresis rod
+    double hyst_vol = 01.4e-8;   // Volume of one hysteresis rod
     double hyst_nd  = 0;        // Demagnetising factor (~0 for L/D > 30 E < 2%)
     int num_x_hyst  = 3;        // --}
     int num_y_hyst  = 3;        // --}}--number of hysteresis rods
@@ -75,12 +155,11 @@ int main()
     */
 
     // Activating Virtual environment
-    venv_activate();
+    // venv_activate();
 
     // Reading and storing magetic fields
     cout << "Reading Magnetic Field Data..." << endl;
-    vector<MagneticEntry> mag_data;
-    mag_data = read_mag_file( inputmagfile, start_time, stop_time);
+    SampleDataVector magData = readMagFile(inputmagfile);
     cout << "Read magnetic field data." << endl
                                         << endl;
 
@@ -94,16 +173,18 @@ int main()
     cout << "Initialised Satellite..." << endl
                                        << endl;
     // Exporting parameters
-    export_params(ahan, outputcsvfile);
+    exportParams(ahan, outputCSVFile);
 
     // Simulating and storing data
-    simulate(ahan, mag_data, timestep, outputcsvfile);
+    // simulate(ahan, magData, start_time, stop_time, timestep, outputCSVFile);
+    simulate(ahan, magData, start_time, stop_time, timestep, outputCSVFile,
+             IntegratorType::Euler, false);
 
     // Plotting data
-    command("python3 ../utils/plot.py " + outputcsvfile);
+    command("python3 ../utils/plot.py " + outputCSVFile);
 
     // Closing Virtual environment
-    venv_deactivate();
+    // venv_deactivate();
 
     return 0;
 }
